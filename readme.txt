e75f8233dbda186bc0219bd1ac21348cce6cd21f
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-06-01 15:27:36 +0300

    u

---
+++ readme.txt
@@ -0,0 +1,21 @@
+
+Task is being implemented in Dancer2.
+
+Problems:
+
+was not possible to do requests from my computer
+to your resource (when trying to get the authentication token). Probably wirewall/country limitations?
+
+
+So it was not possible to test the application
+locally.
+
+reqbin.com worked fine, however. 
+
+Some amount of time within 2 hours was spent on configuring CPAN
+and trying to fix the problem with GET/POST requests from my computer to your
+resource. 
+
+The task is not finished regarding the requirements,
+I am sending what is done within the 2 hours limit.
+
--- task.pl
+++ task.pl
@@ -9,80 +9,90 @@ use JSON::XS qw(decode_json encode_json);
 my $baseUrl = "http://interview.agileengine.com";
 my $imageCache;
 
-my $token;
+my $authToken;
 
 sub makeRequest {
-	my ($url, $method, $headers, $data) = @_;
+    my ($url, $method, $headers, $data) = @_;
 
-	$method ||= 'GET';
+    $method ||= 'GET';
 
-	$headers ||= {};
-	$headers = { 
-		'content-type' => 'application/json', 
-		'Authorization' =>  "Bearer $token",
-		%$headers 
-	};
+    $headers ||= {};
+    $headers = { 
+        'content-type' => 'application/json', 
+        'Authorization' =>  "Bearer $authToken",
+        %$headers 
+    };
 
-	my ($ua,$req,$res,$content);
-	
-	$ua  = LWP::UserAgent->new();	
-	$req = HTTP::Request->new($method => $url);	
-	$req->headers(%$headers);
-	$req->content($data) if $data;
+    my ($ua,$req,$res,$content);
+    
+    $ua  = LWP::UserAgent->new();   
+    $req = HTTP::Request->new($method => $url); 
+    $req->headers(%$headers);
+    $req->content($data) if $data;
 
-	$res = $ua->request($req);
-	
-	return $res;
+    $res = $ua->request($req);
+    
+    return $res;
 }
 
-sub updateCache {
-}
 
 sub updateToken {
-	my $res = makeRequest(
-		$baseUrl . '/auth','POST',{},
-		encode_json({"apiKey" => "23567b218376f79d9415"}),
-	);
+    my $res = makeRequest(
+        $baseUrl . '/auth','POST',{},
+        encode_json({"apiKey" => "23567b218376f79d9415"}),
+    );
 
-	my $data = decode_json($res->content);
+    my $data = decode_json($res->content);
 
-	if ($data && $data->{auth}) {
-		$token = $data->{token};
-	}
+    if ($data && $data->{auth}) {
+        $authToken = $data->{token};
+    }
 }
 
 my $counter = 0;
 my $nTries = 2;
 
 sub getImages {
-	my ($page) = @_;
+    my ($page) = @_;
 
-	my $url = $baseUrl . '/images';
-	$url .= qq{?page=$page} if $page;
+    my $url = $baseUrl . '/images';
+    $url .= qq{?page=$page} if $page;
 
-	my $res = makeRequest($url,'GET');
+    my $res = makeRequest($url,'GET');
 
-	my $data = decode_json($res->content);
-	my $status = $data->{'status'};
+    my $data = decode_json($res->content);
+    my $status = $data->{'status'};
 
-	if($status eq 'Unauthorized') {
-		updateToken();
-		$counter++;
+    if($status eq 'Unauthorized') {
+        updateToken();
+        $counter++;
 
-		if ($counter == $nTries + 1) {
-			warn "maximal number of tries for getImages\n";
-			return;
-		}
-		getImages();
-	}
+        if ($counter == $nTries + 1) {
+            warn "maximal number of tries for getImages\n";
+            return;
+        }
+        return getImages();
+    }
+
+    return $data;
 }
 
+sub updateCache {
+    my $data = getImages();
+
+    my $pageCount = $data->{pageCount};
+    my $hasMore = $data->{hasMore};
+    my $page = $data->{page};
+}
+
+
 sub init {
-	updateToken() unless $token;
+    updateToken() unless $authToken;
+    updateCache() unless $imageCache;
 }
 
 get '/search/:term' => sub {
-	init();
+    init();
 };
 
  
