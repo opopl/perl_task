4f51b03d6801b197e8b33b7a5f827dcf3fb4cc28
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-06-18 01:03:25 -0700

    requirements implemented except periodic updates from the server

--- readme.txt
+++ readme.txt
@@ -1,21 +1,33 @@
 
 Task is being implemented in Dancer2.
 
-Problems:
+18.06.2020, 10:38 am
 
-was not possible to do requests from my computer
-to your resource (when trying to get the authentication token). Probably wirewall/country limitations?
+requirements completed except periodic updating
+from the server. Within the time specified I did not manage
+a find a solution for this within the Dancer2 web framework,
+since it itself runs the built-in within a loop (dance() function at the end
+of the program). I am using Windows, not Linux. Probably on Linux it is easier
+to implement? 
 
+- cache is working
+= route /search/:term is working.
 
-So it was not possible to test the application
-locally.
+259 pictures were loaded at 10:48.
+examples of usage:
 
-reqbin.com worked fine, however. 
+http://localhost:3000/search/nikon
+http://localhost:3000/wonderful/nikon
 
-Some amount of time within 2 hours was spent on configuring CPAN
-and trying to fix the problem with GET/POST requests from my computer to your
-resource. 
+search is case-insensitive. 
 
-The task is not finished regarding the requirements,
-I am sending what is done within the 2 hours limit.
+Run on Windows 7, strawberry perl 64-bit, 5.24.4
+
+Considerations for the requirement of periodic updates: 
+	use other web-server or framework, not Dancer2 ? Use AnyEvent or $SIG{ALARM} = ...
+	trick? I am using Windows, I do not have a Linux machine at home,
+	probably it is easier to do in linux?
+
+	One could also do periodic updating from outside the script, e.g.
+	via crontab on linux
 
--- task.pl
+++ task.pl
@@ -5,11 +5,23 @@ use Dancer2;
 use LWP;
 use HTTP::Request;
 use JSON::XS qw(decode_json encode_json);
+use Data::Dumper;
+use DBI;
+use FindBin qw($Bin);
+use AnyEvent;
+
+my @searchFields = qw(tag author camera);
 
 my $baseUrl = "http://interview.agileengine.com";
 my $imageCache;
 
 my $authToken;
+my $dbh;
+
+# for testing, maximal number of pics
+#   to be loaded from the external server upon initialization
+my $numPics;
+my $picNum=0;
 
 sub makeRequest {
     my ($url, $method, $headers, $data) = @_;
@@ -17,21 +29,26 @@ sub makeRequest {
     $method ||= 'GET';
 
     $headers ||= {};
+    $headers->{'Authorization'} = "Bearer $authToken" if $authToken;
+
     $headers = { 
-        'content-type' => 'application/json', 
-        'Authorization' =>  "Bearer $authToken",
+        'Content-Type' => 'application/json', 
         %$headers 
     };
 
     my ($ua,$req,$res,$content);
     
     $ua  = LWP::UserAgent->new();   
+    $ua->agent('Mozilla/8.0');
+
     $req = HTTP::Request->new($method => $url); 
-    $req->headers(%$headers);
+    $req->header(%$headers);
     $req->content($data) if $data;
 
     $res = $ua->request($req);
-    
+
+    print Dumper($res->as_string) . "\n";
+
     return $res;
 }
 
@@ -53,17 +70,18 @@ my $counter = 0;
 my $nTries = 2;
 
 sub getImages {
-    my ($page) = @_;
+    my ($page,$id) = @_;
 
     my $url = $baseUrl . '/images';
     $url .= qq{?page=$page} if $page;
+    $url .= qq{/$id} if $id;
 
     my $res = makeRequest($url,'GET');
 
     my $data = decode_json($res->content);
     my $status = $data->{'status'};
 
-    if($status eq 'Unauthorized') {
+    if($status && $status eq 'Unauthorized') {
         updateToken();
         $counter++;
 
@@ -71,28 +89,209 @@ sub getImages {
             warn "maximal number of tries for getImages\n";
             return;
         }
-        return getImages();
+        return getImages($page,$id);
     }
 
     return $data;
 }
 
+sub initDatabase(){
+    my $dbname = $Bin . '/task.db';
+    $dbh = DBI->connect('dbi:SQLite:dbname='.$dbname,
+        '','',{AutoCommit=>1,RaiseError=>1,PrintError=>0}
+    );
+
+    my $q = q{
+        CREATE TABLE IF NOT EXISTS pictures (
+            id TEXT,
+            cropped_picture TEXT,
+            author TEXT,
+            camera TEXT,
+            tag TEXT,
+            full_picture TEXT
+        )
+    };
+    $dbh->do($q);
+}
+
+sub insertPictureDB {
+    my ($pic) = @_;
+
+    my @fields = keys %$pic;
+    my $fieldString = join("," => @fields);
+    my $quoteString = join("," => ( map { '?' } @fields ));
+
+    my $sth;
+
+    #do not insert tags repeatedly
+    my $tag = $pic->{tag};
+    my $id = $pic->{id};
+
+    $sth = $dbh->prepare(qq{SELECT COUNT(*) FROM pictures WHERE tag = ? AND id = ? });
+    $sth->execute($tag, $id);
+    my ($count) = $sth->fetchrow_array;
+
+    return if $count;
+
+    my $q = qq{
+        INSERT INTO pictures ($fieldString) VALUES ($quoteString)
+    };
+
+    $sth = $dbh->prepare($q);
+    $sth->execute(@$pic{@fields});
+}
+
+sub insertPicture {
+    my ($pic) = @_;
+
+    # for testing purposes
+    $picNum++;
+    return if $numPics && $picNum > $numPics;
+    print 'Inserting picture number ' . $picNum . "\n";
+
+    my $id = $pic->{id};
+
+    my $data = getImages(undef,$id);
+    $pic = { %$pic, %$data } if $data;
+
+    my $tagString = $pic->{tags};
+    my @tags;
+    if ($tagString) {
+        @tags = split /\s+/ => $tagString;
+        s/^#//g for(@tags); 
+    }
+
+    delete $pic->{tags};
+
+    if (@tags) {
+        foreach my $tag (@tags) {
+            $pic->{tag}=$tag;
+            insertPictureDB($pic);
+        }
+    }else{
+        insertPictureDB($pic);
+    }
+
+}
+
 sub updateCache {
-    my $data = getImages();
+    my ($page,$id,$maxPage,$maxEntries) = @_;
+
+    my $data = getImages($page,$id);
+    $page ||= $data->{page};
 
     my $pageCount = $data->{pageCount};
-    my $hasMore = $data->{hasMore};
-    my $page = $data->{page};
+    my $hasMore   = $data->{hasMore};
+    my $pics      = $data->{pictures};
+
+    $maxPage ||= $pageCount;
+
+    foreach my $pic (@$pics) {
+        insertPicture($pic);
+    }
+
+    if ($page == 1 && $hasMore && $maxPage && $maxPage > 1) {
+        my @pages = ( 2 .. $maxPage );
+        foreach my $p (@pages) {
+            updateCache($p);
+        }
+    }
+
 }
 
 
 sub init {
+    initDatabase() unless $dbh;
     updateToken() unless $authToken;
     updateCache() unless $imageCache;
 }
 
+sub searchTerm {
+    my $term = route_parameters->get('term');
+    
+    my $w = join(" OR ", 
+        map { qq{
+            $_ LIKE '%$term%' 
+                OR $_ LIKE '$term%'
+                OR $_ LIKE '%$term'
+            } 
+        } @searchFields );
+
+    my $q = qq{SELECT * FROM pictures WHERE $w };
+
+    my $sth = $dbh->prepare($q);
+    $sth->execute();
+
+    my $data={};
+    while(my $row = $sth->fetchrow_hashref){
+        my $id = $row->{id};
+        my $tag = $row->{tag};
+
+        delete $row->{tag};
+
+        $data->{$id} ||= $row;
+
+        unless($data->{$id}->{tags}){
+            my $sth = $dbh->prepare(qq{
+                SELECT tag FROM pictures WHERE id = ?
+            });
+            $sth->execute($id);
+            my $tags = $sth->fetchall_arrayref;
+            $data->{$id}->{tags} .= 
+                join(" ",map { '#' . shift @$_ } @$tags );
+        }
+
+    }
+
+    my @pictures;
+    foreach my $id (keys %$data) {
+        push @pictures, $data->{$id};
+    }
+    return encode_json({ 
+       'pictures' => \@pictures,
+       'count'    => scalar @pictures
+    });
+
+}
+
+init();
+
+#local $SIG{ALRM} = sub {
+    #init();
+    #alarm 1;
+
+#};
+#alarm 1;
+
+#my $w = AnyEvent->timer(
+    #after => 1000, 
+    #interval => 4000, 
+    #cb => sub { 
+        #init();
+    #}
+#); 
+    #
+#get '/pictures' => sub {
+    #my $q = qq{SELECT * FROM pictures};
+    #my $sth = $dbh->prepare($q);
+    #$sth->execute();
+
+    #my @pictures; 
+    #while (my $row = $sth->fetchrow_hashref) {
+        #my $id = $row->{id};
+        #push @pictures, $row;
+    #}
+
+    #return encode_json({ 
+       #'pictures' => \@pictures,
+       #'count'    => scalar @pictures
+    #});
+#};
+    #
+    
+
 get '/search/:term' => sub {
-    init();
+    searchTerm();
 };
 
  
